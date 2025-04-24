35b0f7187ea1a1e140e0deafd9df3c65f1a819cb
GuilFranca <guilfranca27@gmail.com>
2025-04-24 19:16:31 -0300

    finaliando sidebar - 40:29

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navBar = document.querySelector('.navbar');
 const searchBox = document.querySelector('.search-box .bx-search');
 
+// Barra de pesquisa
+
 searchBox.addEventListener('click', () => {
     navBar.classList.toggle("showInput");
 
@@ -10,3 +12,38 @@ searchBox.addEventListener('click', () => {
         searchBox.classList.replace("bx-x", "bx-search");
     }
 })
+
+// sidebar open close
+
+let menuOpenBtn = document.querySelector('.navbar .bx-menu');
+let closeOpenBtn = document.querySelector('.nav-links .bx-x');
+let navLinks = document.querySelector('.nav-links');
+
+menuOpenBtn.addEventListener("click", ()=>{
+    navLinks.style.left = "0";
+})
+
+closeOpenBtn.addEventListener("click", ()=>{
+    navLinks.style.left = "-100%";
+})
+
+// sidebar submenu
+
+let htmlcssArrow = document.querySelector('.htmlcss-arrow');
+
+htmlcssArrow.addEventListener('click', ()=>{
+    navLinks.classList.toggle('show1');
+});
+
+let moreArrow = document.querySelector('.more-arrow');
+
+moreArrow.addEventListener('click', ()=>{
+    navLinks.classList.toggle('show2');
+});
+
+let jsArrow = document.querySelector('.js-arrow');
+
+jsArrow.addEventListener('click', ()=>{
+    navLinks.classList.toggle('show3');
+});
+
